8ce156f7bddd0eff3d1a9a0dd370b73cb6b7dc63
Marco Baygorria <arrancar1991@live.cl>
2024-03-07 21:07:08 -1100

    Antes de insertar por lote

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -7,6 +7,8 @@ export class SeedService {
 
   private readonly axios: AxiosInstance = axios
 
+
+
   async executeSeed() {
 
     const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
