aa15a1f3e253714ac8d990bcf8aab623298e039f
Marco Baygorria <arrancar1991@live.cl>
2024-03-07 21:06:43 -1100

    Antes de insertar por lote

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,15 @@ import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'), // conexion a BBDD
-    PokemonModule, CommonModule
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'), // conexión a BBDD
+    PokemonModule, CommonModule, SeedModule
   ],
 })
 export class AppModule { }
